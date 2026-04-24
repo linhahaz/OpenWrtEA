b66815bd9f7be5c7ecbab4a43de4e83383fa8387
linhahaz <49563001+linhahaz@users.noreply.github.com>
2026-04-25 05:32:48 +0800

    Update diy-part2-router.sh

--- diy-part2-router.sh
+++ diy-part2-router.sh
@@ -58,7 +58,7 @@ function clean_packages(){
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
@@ -114,7 +114,7 @@ config_package_add luci-app-upnp
 # tty 终端
 config_package_add luci-app-ttyd
 # docker
-config_package_add luci-app-dockerman
+config_package_del luci-app-dockerman
 # kms
 config_package_add luci-app-vlmcsd
 # usb 2.0 3.0 支持
@@ -129,51 +129,22 @@ config_package_add kmod-usb-serial-option
 config_package_add kmod-usb-net-rndis
 config_package_add kmod-usb-net-ipheth
 
-# 第三方软件包
-mkdir -p package/custom
-git clone --depth 1  https://github.com/217heidai/OpenWrt-Packages.git package/custom
-clean_packages package/custom
-## golang
-rm -rf feeds/packages/lang/golang
-mv package/custom/golang feeds/packages/lang/
 ## argon 主题
 config_package_add luci-theme-argon
-## passwall
-config_package_add luci-app-passwall
-config_package_add luci-app-passwall_Nftables_Transparent_Proxy
-config_package_del luci-app-passwall_Iptables_Transparent_Proxy
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Libev_Client
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Libev_Server
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Client
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Server
-config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Client
-config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Server
-## 定时任务。重启、关机、重启网络、释放内存、系统清理、网络共享、关闭网络、自动检测断网重连、MWAN3负载均衡检测重连、自定义脚本等10多个功能
-config_package_add luci-app-taskplan
-config_package_add luci-lib-ipkg
+## homeproxy
+config_package_add luci-app-homeproxy
+config_package_add luci-i18n-homeproxy-zh-cn
+## 新增
+config_package_add luci-app-autoreboot
 ## 分区扩容。一键自动格式化分区、扩容、自动挂载插件，专为OPENWRT设计，简化OPENWRT在分区挂载上烦锁的操作
 config_package_add luci-app-partexp
-## iStore 应用市场
-#config_package_add luci-app-store
-## 4G/5G 支持：FM350-GL USB RNDIS
-### Siriling/5G-Modem-Support
-config_package_add luci-app-modem
-### luci-app-modemband
-config_package_add luci-app-modemband
-### luci-app-3ginfo-lite
-config_package_add luci-app-3ginfo-lite
-### luci-app-sms-tool-js
-config_package_add luci-app-sms-tool-js
-## luci-app-easytier
-config_package_add luci-app-easytier
-config_package_add easytier
 
 # 镜像生成
 # 修改分区大小
 sed -i "/CONFIG_TARGET_KERNEL_PARTSIZE/d" .config
 echo "CONFIG_TARGET_KERNEL_PARTSIZE=32" >> .config
 sed -i "/CONFIG_TARGET_ROOTFS_PARTSIZE/d" .config
-echo "CONFIG_TARGET_ROOTFS_PARTSIZE=2048" >> .config
+echo "CONFIG_TARGET_ROOTFS_PARTSIZE=1024" >> .config
 # 调整 GRUB_TIMEOUT
 sed -i "s/CONFIG_GRUB_TIMEOUT=\"3\"/CONFIG_GRUB_TIMEOUT=\"1\"/" .config
 ## 不生成 EXT4 硬盘格式镜像
