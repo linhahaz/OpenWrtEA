1c4aed9c4184964682e63667075342b675c0615a
linhahaz <49563001+linhahaz@users.noreply.github.com>
2026-04-30 11:14:40 +0800

    Update diy-part2-router.sh

--- diy-part2-router.sh
+++ diy-part2-router.sh
@@ -38,56 +38,41 @@ function config_package_add(){
     config_add $package
 }
 
-function drop_package(){
-    if [ "$1" != "golang" ];then
-        # feeds/base -> package
-        find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
-        find feeds/ -follow -name $1 -not -path "feeds/base/custom/*" | xargs -rt rm -rf
-    fi
-}
-function clean_packages(){
-    path=$1
-    dir=$(ls -l ${path} | awk '/^d/ {print $NF}')
-    for item in ${dir}
-        do
-            drop_package ${item}
-        done
-}
-
 # Add the default password for the 'root' user（Change the empty password to 'password'）
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 删除
 # Firmware
-config_package_del intel-igpu-firmware-dmc
-# LED modules
-config_package_del kmod-ledtrig-audio
+#config_package_del i915-firmware-dmc
 # Sound Support
 config_package_del kmod-sound-core
-config_package_del kmod-ac97
-config_package_del kmod-sound-hda-core
-config_package_del kmod-sound-hda-codec-hdmi
-config_package_del kmod-sound-hda-codec-realtek
-config_package_del kmod-sound-hda-codec-via
-config_package_del kmod-sound-hda-intel
-config_package_del kmod-sound-i8x0
-config_package_del kmod-sound-mpu401
-config_package_del kmod-sound-via82xx
-config_package_del kmod-usb-audio
-# Sound
-config_package_del alsa-ucm-conf
-config_package_del alsa-lib
-config_package_del alsa-utils
+# Video Support
+config_package_del kmod-acpi-video
+config_package_del kmod-backlight
+config_package_del kmod-drm
+config_package_del kmod-drm-buddy
+config_package_del kmod-drm-display-helper
+config_package_del kmod-drm-exec
+config_package_del kmod-drm-i915
+config_package_del kmod-drm-kms-helper
+config_package_del kmod-drm-suballoc-helper
+config_package_del kmod-drm-ttm
+config_package_del kmod-drm-ttm-helper
+config_package_del kmod-fb
+config_package_del kmod-fb-cfb-copyarea
+config_package_del kmod-fb-cfb-fillrect
+config_package_del kmod-fb-cfb-imgblt
+config_package_del kmod-fb-sys-fops
+config_package_del kmod-fb-sys-ram
 # Other
 config_package_del luci-app-rclone_INCLUDE_rclone-webui
 config_package_del luci-app-rclone_INCLUDE_rclone-ng
-
 # 新增
 # Firmware
 config_package_add intel-microcode
@@ -112,9 +97,10 @@ config_package_add luci-app-upnp
 # tty 终端
 config_package_add luci-app-ttyd
 # docker
-config_package_add luci-app-dockerman
-# kms
-config_package_add luci-app-vlmcsd
+config_package_del luci-app-dockerman
+# tun
+config_package_add kmod-tun
+config_package_add ip-full
 # usb 2.0 3.0 支持
 config_package_add kmod-usb2
 config_package_add kmod-usb3
@@ -126,52 +112,42 @@ config_package_add kmod-usb-serial
 config_package_add kmod-usb-serial-option
 config_package_add kmod-usb-net-rndis
 config_package_add kmod-usb-net-ipheth
-
 # 第三方软件包
-mkdir -p package/custom
-git clone --depth 1  https://github.com/217heidai/OpenWrt-Packages.git package/custom
-clean_packages package/custom
-## golang
+
+## 替换 Golang
 rm -rf feeds/packages/lang/golang
-mv package/custom/golang feeds/packages/lang/
-## argon 主题
-config_package_add luci-theme-argon
-## passwall
-config_package_add luci-app-passwall
-config_package_add luci-app-passwall_Nftables_Transparent_Proxy
-config_package_del luci-app-passwall_Iptables_Transparent_Proxy
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Libev_Client
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Libev_Server
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Client
-config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Server
-config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Client
-config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Server
-## 定时任务。重启、关机、重启网络、释放内存、系统清理、网络共享、关闭网络、自动检测断网重连、MWAN3负载均衡检测重连、自定义脚本等10多个功能
-config_package_add luci-app-taskplan
-config_package_add luci-lib-ipkg
-## 分区扩容。一键自动格式化分区、扩容、自动挂载插件，专为OPENWRT设计，简化OPENWRT在分区挂载上烦锁的操作
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+
+# OpenWrt-momo
+# git clone https://github.com/nikkinikki-org/OpenWrt-momo.git package/OpenWrt-momo
+
+## MosDNS v5
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/v2ray-geodata
+
+# 克隆源码到 package 目录
+git clone --depth 1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+## 新增插件
+config_package_add luci-app-mosdns
+config_package_add mosdns
+config_package_add luci-i18n-mosdns-zh-cn
+config_package_add luci-app-homeproxy
+config_package_add luci-i18n-homeproxy-zh-cn
+config_package_add luci-app-autoreboot
 config_package_add luci-app-partexp
-## iStore 应用市场
-#config_package_add luci-app-store
-## 4G/5G 支持：FM350-GL USB RNDIS
-### Siriling/5G-Modem-Support
-config_package_add luci-app-modem
-### luci-app-modemband
-config_package_add luci-app-modemband
-### luci-app-3ginfo-lite
-config_package_add luci-app-3ginfo-lite
-### luci-app-sms-tool-js
-config_package_add luci-app-sms-tool-js
-## luci-app-easytier
-config_package_add luci-app-easytier
-config_package_add easytier
+# config_package_add momo
+# config_package_add luci-app-momo
+# config_package_add luci-i18n-momo-zh-cn
 
 # 镜像生成
 # 修改分区大小
 sed -i "/CONFIG_TARGET_KERNEL_PARTSIZE/d" .config
 echo "CONFIG_TARGET_KERNEL_PARTSIZE=32" >> .config
 sed -i "/CONFIG_TARGET_ROOTFS_PARTSIZE/d" .config
-echo "CONFIG_TARGET_ROOTFS_PARTSIZE=2048" >> .config
+echo "CONFIG_TARGET_ROOTFS_PARTSIZE=1024" >> .config
 # 调整 GRUB_TIMEOUT
 sed -i "s/CONFIG_GRUB_TIMEOUT=\"3\"/CONFIG_GRUB_TIMEOUT=\"1\"/" .config
 ## 不生成 EXT4 硬盘格式镜像
