f723ed62cd8a33e439bfc372cd776984fcea4bf8
linhahaz <49563001+linhahaz@users.noreply.github.com>
2026-06-05 01:18:51 +0800

    Update diy-part2-router.sh

--- diy-part2-router.sh
+++ diy-part2-router.sh
@@ -38,20 +38,19 @@ function config_package_add(){
     config_add $package
 }
 
-# Add the default password for the 'root' user（Change the empty password to 'password'）
-sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+# 取消默认密码（恢复空密码直接登录）
+# sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # Modify default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 删除
 # Firmware
 #config_package_del i915-firmware-dmc
-# Sound Support
-config_package_add kmod-sound-core
+# CAKE 算法支持
 config_package_add kmod-sched-cake
 # Video Support
 config_package_del kmod-acpi-video
@@ -95,17 +94,10 @@ config_package_add default-settings-chn
 config_package_add kmod-tcp-bbr
 # coremark cpu 跑分
 config_package_add coremark
-# autocore + lm-sensors-detect： cpu 频率、温度
+# autocore： cpu 频率与基础状态显示
 config_package_add autocore
-config_package_add lm-sensors-detect
 # nano 替代 vim
 config_package_add nano
-# upnp
-config_package_add luci-app-upnp
-# python3
-#config_package_add python3
-#config_package_add python3-base
-#config_package_add python3-pip
 # tty 终端
 config_package_add luci-app-ttyd
 # tun
@@ -114,52 +106,16 @@ config_package_add ip-full
 config_package_add kmod-nft-socket
 config_package_add kmod-nft-tproxy
 config_package_add kmod-nft-nat
-# usb 2.0 3.0 支持
-config_package_add kmod-usb2
-config_package_add kmod-usb3
-# usb 网络支持
-config_package_add usbmuxd
-config_package_add usbutils
-config_package_add usb-modeswitch
-config_package_add kmod-usb-serial
-config_package_add kmod-usb-serial-option
-config_package_add kmod-usb-net-rndis
-config_package_add kmod-usb-net-ipheth
-
-# 第三方软件包
-rm -rf feeds/luci/applications/luci-app-ssr-plus
-rm -rf feeds/packages/net/{xray-core,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tuic-client,v2ray-plugin,xray-plugin,shadow-tls}
-git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 
 # 保留原生的 offload 作为备用
 config_package_add kmod-nft-offload
 
-## 替换 Golang
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
-
-# OpenWrt-momo
-# git clone https://github.com/nikkinikki-org/OpenWrt-momo.git package/OpenWrt-momo
-
-## MosDNS v5
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/v2ray-geodata
-
-# 克隆源码到 package 目录
-git clone --depth 1 https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone --depth 1 https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-
 ## 新增插件
-config_package_add luci-app-mosdns
-config_package_add mosdns
-config_package_add luci-i18n-mosdns-zh-cn
 config_package_add luci-app-homeproxy
 config_package_add luci-i18n-homeproxy-zh-cn
-config_package_add luci-app-autoreboot
 
 # 系统底层优化 (BBR + 时区 + 禁用IPv6)
-# 强制开启 BBR
+# 强制开启 cake
 echo "net.core.default_qdisc=cake" >> package/base-files/files/etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> package/base-files/files/etc/sysctl.conf
 
